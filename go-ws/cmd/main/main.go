4e9bcfddcb799cead528d4cf7c317fdbfa7d8ac5
Alex Straight <alex@Alexs-Laptop.local>
2024-06-30 17:01:16 -0700

    ...

--- go-ws/cmd/main/main.go
+++ go-ws/cmd/main/main.go
@@ -80,8 +80,9 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
 	log.Printf("Listening on port %s", port)
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":"+port, r)
+
 }
