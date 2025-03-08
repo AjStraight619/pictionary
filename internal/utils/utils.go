dc45977e1e07e273a8fc8a132a63eaba09360f2f
Alex Straight <AlexStraight619@gmail.com>
2025-03-07 21:06:47 -0800

    Add helper functions Delay for setting delays between game events. CreateMessage for preparing message type and payload

---
+++ internal/utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/json"
+	"time"
+)
+
+func CreateMessage(msgType string, payload interface{}) ([]byte, error) {
+	message := map[string]interface{}{
+		"type":    msgType,
+		"payload": payload,
+	}
+
+	b, err := json.Marshal(message)
+
+	return b, err
+}
+
+func Delay(delay time.Duration, fn func()) {
+	go func() {
+		time.Sleep(delay)
+		fn()
+	}()
+}
